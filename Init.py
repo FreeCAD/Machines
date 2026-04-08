fa4b2b628c8abd36bf9d6d9a5cfb070827f18572
sliptonic <shopinthewoods@gmail.com>
2026-04-08 09:47:40 -0500

    remove unneeded Init.py

--- Init.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: CC-BY-SA-4.0
-# SPDX-FileNotice: Part of the Machines addon.
-#
-# This is a pure data addon. Machine definitions and post-processors are
-# discovered by CAM via the <content> metadata in package.xml.
-# No executable registration code is needed here.
